2343d2d53aea10c236a489aa34b4a6a1eb75e9b1
Shouvik Roy <shouvik@techie.com>
2018-05-04 00:38:05 +0530

    fix whitespace formatting

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -197,7 +197,7 @@ module.exports = {
                 options: {
                   importLoaders: 1,
                   modules: true,
-							    localIdentName: '[name]__[local]___[hash:base64:5]'
+                  localIdentName: '[name]__[local]___[hash:base64:5]'
                 },
               },
               require.resolve('sass-loader')
